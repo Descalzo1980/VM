cde200f64dae4a4ec6f06dcf9c5c1b76c6b5fc37
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-12-28 23:11:44 +0300

    LDVM

--- app/src/main/java/ru/stas/vm/MainActivity.kt
+++ app/src/main/java/ru/stas/vm/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.stas.vm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import ru.stas.vm.databinding.ActivityMainBinding
 
@@ -9,18 +10,37 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val viewModel: MainActivityViewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+        /*ViewModel+LiveCycle****************************************************************/
+        viewModel.startCounter()
 
+        binding.button1.setOnClickListener {
+            viewModel.seconds().observe(this, Observer {
+                binding.textView1.text = it.toString()
+            })
+        }
+
+
+        viewModel.finished().observe(this) {
+            if (it) {
+                binding.textView1.text = it.toString()
+            }
+        }
+        /*ViewModel****************************************************************/
         binding.textView.text = viewModel.number.toString()
 
         binding.button.setOnClickListener {
             viewModel.addNumber()
             binding.textView.text = viewModel.number.toString()
         }
+
+
+
     }
 }
--- app/src/main/java/ru/stas/vm/MainActivityViewModel.kt
+++ app/src/main/java/ru/stas/vm/MainActivityViewModel.kt
@@ -1,12 +1,40 @@
 package ru.stas.vm
 
+import android.os.CountDownTimer
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel: ViewModel() {
 
     var number = 0
 
+    val seconds = MutableLiveData<Int>()
+    val finished = MutableLiveData<Boolean>()
+
     fun addNumber(){
         number++
+    }
+
+
+    fun seconds():LiveData<Int>{
+        return seconds
+    }
+
+    fun finished(): LiveData<Boolean> {
+        return finished
+    }
+
+    fun startCounter(){
+        object : CountDownTimer(15000, 100) {
+            override fun onTick(millisUntilFinished: Long) {
+                val time = millisUntilFinished / 1000
+
+                seconds.value = time.toInt()
+            }
+            override fun onFinish() {
+                finished.value = true
+            }
+        }.start()
     }
 }
